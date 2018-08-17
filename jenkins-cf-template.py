d6439bf89dbfd7adbaa1506c6e7be545423405a7
root <root@DESKTOP-KAFTHQ5.localdomain>
2018-08-17 20:09:25 +0900

    Allowing Jenkins to interact with CodePipeline

--- jenkins-cf-template.py
+++ jenkins-cf-template.py
@@ -94,6 +94,19 @@ t.add_resource(InstanceProfile(
   Roles=[Ref("Role")]
 ))
 
+t.add_resource(IAMPolicy("Policy",
+  PolicyName="AllowCodePipeline",
+  PolicyDocument=Policy(
+    Statement=[
+      Statement(Effect=Allow,
+        Action=[Action("codepipeline", "*")],
+        Resource=["*"]
+      )
+    ]
+  ),
+  Roles=[Ref("Role")]
+))
+
 t.add_resource(ec2.Instance(
     "instance",
     ImageId="ami-ebc47185",
